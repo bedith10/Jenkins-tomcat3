b991af4b894f3d21b33a76e187aca3f7de0cf558
Serge <serge@gmail.com>
2021-09-30 15:46:51 -0500

    mew repo

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -89,6 +89,18 @@ service jenkins start
    fi
 
 chkconfig jenkins on
+########################
+# Docker
+########################
+echo "Installing Docker"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get -y install git docker-ce
+sudo systemctl enable docker
+sudo usermod -aG docker ${USER}
+sudo usermod -aG docker jenkins
+sudo usermod -aG docker ubuntu
 
 #echo -e "\n Installing ip tables package...\n"
 
